3ae81be07270665b49692960049d88b6137b88ae
dannylmathews <dannyl.mathews@gmail.com>
2015-04-06 10:50:11 -0700

    v1

--- W5D1 - ImagePicker/ViewController.m
+++ W5D1 - ImagePicker/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+
+@property (strong, nonatomic) UIImagePickerController *imagePicker;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -17,11 +20,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.imagePicker = [[UIImagePickerController alloc] init];
+    self.imagePicker.delegate = self;
+    
 }
+
+- (IBAction)pressedAdd:(id)sender {
+    
+    UIAlertController *actionSheet = [UIAlertController alertControllerWithTitle:@"Choose Photo" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]){
+        UIAlertAction *camera = [UIAlertAction actionWithTitle:@"Camera" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            self.imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+            [self presentViewController:self.imagePicker animated:YES completion:nil];
+        }];
+        
+        [actionSheet addAction:camera];
+    }
+    
+    UIAlertAction *galleryAction = [UIAlertAction actionWithTitle:@"From Gallery" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        self.imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        [self presentViewController:self.imagePicker animated:YES completion:nil];
+    }];
+    
+    [actionSheet addAction:galleryAction];
+
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel   handler:^(UIAlertAction *action) {
+        
+    }];
+    
+    [actionSheet addAction:cancelAction];
+    
+    [self presentViewController:actionSheet animated:YES completion:nil];
+
+
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    
+    UIImage *originalImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+    
+    if ([picker sourceType] == UIImagePickerControllerSourceTypeCamera) {
+        // Do something with an image from the camera
+        UIImageWriteToSavedPhotosAlbum(originalImage, nil, nil, nil);
+    }
+    
+    [self.imageView setContentMode:UIViewContentModeScaleAspectFit];
+    self.imageView.image = originalImage;
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
 }
 
 @end
